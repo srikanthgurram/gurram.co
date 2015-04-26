0e37851ba83946700eba37343c229143e0f601ad
Srikanth Gurram <2gurram@gmail.com>
2015-04-26 18:29:56 +0530

    added HOME controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :home
+  root 'home#index'
   # Example resource route with options:
   #   resources :products do
   #     member do
