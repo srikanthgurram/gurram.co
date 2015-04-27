c0ce9361f0438ad7c510e68a1fc616a1e3feb137
Srikanth Gurram <2gurram@gmail.com>
2015-04-27 12:10:56 +0530

    static asserts set true in config file

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module PersonalSite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Static assets
+    config.serve_static_assets = true
   end
 end
